53d7e95f6b20263d8d59c5afcac0a46460bb9e91
@k <kale3930@gmail.com>
2021-03-30 19:23:45 -0400

    Set up arg parsing template and todo list objects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+
+struct TodoItem {
+    name: String,
+    completed: char
+}
+
+impl TodoItem {
+    fn new(name: String) -> TodoItem {
+        return TodoItem {
+            name,
+            completed: ' '
+        };
+    }
+}
+
+struct TodoList{
+    list: Vec<TodoItem>
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        return TodoList{list: Vec::new()}
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    let command = arguments[1].clone();
+    
+    let todo_item = TodoItem::new("Say_hi".to_string());
+    let todo_list = vec![todo_item];
+
+    if command == "get" {
+        for item in todo_list {
+            println!("[{}] - {}", item.completed, item.name);
+        }
+    }
 }
